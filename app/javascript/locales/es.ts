6a004eb0cc3fd9164aa79f85263162256924a7e8
Gabriel San Martin <gsanmartinandaur@gmail.com>
2022-10-25 00:54:04 -0300

    feat(locales/es): add translations for multiple views

--- app/javascript/locales/es.ts
+++ app/javascript/locales/es.ts
@@ -1,9 +1,35 @@
 export default {
+  user: {
+    title: 'Usuario',
+  },
+  session: {
+    signin: 'Iniciar sesión',
+    signout: 'Cerrar sesión',
+    signup: 'Registrarse'
+  },
   home: {
     title: 'Inicio',
   },
+  inventory:{
+    title: 'Inventario',
+  },
+  menu:{
+    title: 'Menú',
+  },
   recipes: {
     title: "Recetas",
+  },
+  tables: {
+    title: "Mesas",
+  },
+  clients: {
+    title: "Clientes",
+  },
+  reservations: {
+    title: "Reservaciones",
+  },
+  finances: {
+    title: "Finanzas",
   },
   actions: {
     back: 'Atrás',
