1157414507930893f5bb7ff9f6611ae873e11f90
Gabriel San Martin <gsanmartinandaur@gmail.com>
2022-10-25 00:54:50 -0300

    chore(headlessui): test headlessUI components in views

--- app/javascript/views/home-view.vue
+++ app/javascript/views/home-view.vue
@@ -1,18 +1,25 @@
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n';
+  import { ref } from 'vue'
+  import {
+    Listbox,
+    ListboxButton,
+    ListboxOptions,
+    ListboxOption,
+  } from '@headlessui/vue'
+  const { t } = useI18n({});
+
+  const people = [
+    { id: 1, name: 'Durward Reynolds', unavailable: false },
+    { id: 2, name: 'Kenton Towne', unavailable: false },
+    { id: 3, name: 'Therese Wunsch', unavailable: false },
+    { id: 4, name: 'Benedict Kessler', unavailable: true },
+    { id: 5, name: 'Katelyn Rohan', unavailable: false },
+  ]
+  const selectedPerson = ref(people[0])
+</script>
 <template>
   <div>
-    {{ this.hello }}
+    {{ t('home.title') }}
   </div>
 </template>
-<script lang="ts">
-export default {
-  props: {
-    hello: {
-      type: String,
-      default: ''
-    }
-  }
-}
-</script>
-<style lang="">
-
-</style>
--- app/javascript/views/recipes-view.vue
+++ app/javascript/views/recipes-view.vue
@@ -1,13 +1,38 @@
-<template lang="">
-  <div class="bg-red-200">
-    {{$t('recipes.title')}}
-  </div>
-</template>
 <script setup lang="ts">
   import { useI18n } from 'vue-i18n';
+  import {
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+    DialogDescription,
+  } from '@headlessui/vue'
+  import { ref } from 'vue'
+
+  const isOpen = ref(true)
+
+  function setIsOpen(value : boolean) {
+    isOpen.value = value
+  }
 
   const { t } = useI18n({});
 </script>
-<style lang="">
+<template lang="">
+  <div class="bg-red-200">
+    {{$t('recipes.title')}}
+    <Dialog :open="isOpen" @close="setIsOpen" class="relative z-50">
+    <!-- The backdrop, rendered as a fixed sibling to the panel container -->
+    <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
 
-</style>
+    <!-- Full-screen container to center the panel -->
+    <div class="fixed inset-0 flex items-center justify-center p-4">
+      <!-- The actual dialog panel -->
+      <DialogPanel class="w-full max-w-sm rounded bg-white">
+        <DialogTitle>Complete your order</DialogTitle>
+
+        <!-- ... -->
+      </DialogPanel>
+    </div>
+  </Dialog>
+  <button @click="setIsOpen(true)">Open Dialog</button>
+  </div>
+</template>
