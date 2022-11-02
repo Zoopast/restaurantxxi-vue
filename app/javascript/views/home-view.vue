4b464df778af8ad4d5fcd55993ef90db204e1ac5
Gabriel San Martin <gsandaur@hotmail.com>
2022-11-02 18:03:19 -0300

    refactor(home-view): delete unused headlessui component in home view

--- app/javascript/views/home-view.vue
+++ app/javascript/views/home-view.vue
@@ -1,22 +1,7 @@
 <script setup lang="ts">
   import { useI18n } from 'vue-i18n';
-  import { ref } from 'vue'
-  import {
-    Listbox,
-    ListboxButton,
-    ListboxOptions,
-    ListboxOption,
-  } from '@headlessui/vue'
-  const { t } = useI18n({});
 
-  const people = [
-    { id: 1, name: 'Durward Reynolds', unavailable: false },
-    { id: 2, name: 'Kenton Towne', unavailable: false },
-    { id: 3, name: 'Therese Wunsch', unavailable: false },
-    { id: 4, name: 'Benedict Kessler', unavailable: true },
-    { id: 5, name: 'Katelyn Rohan', unavailable: false },
-  ]
-  const selectedPerson = ref(people[0])
+  const { t } = useI18n({});
 </script>
 <template>
   <div>
