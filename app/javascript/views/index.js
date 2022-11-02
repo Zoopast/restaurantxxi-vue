929a37f4b0f5856d5c3cee0058f28c50d0657420
Gabriel San Martin <gsandaur@hotmail.com>
2022-11-02 18:03:54 -0300

    feat(javascript/views): add index.js for Components export

---
+++ app/javascript/views/index.js
@@ -0,0 +1,29 @@
+import HomeView from "./home-view.vue";
+import ExpensesView from "./expenses-view.vue";
+import FinancesView from "./finances-view.vue";
+import InventoryView from "./inventory-view.vue";
+import ItemsView from "./items-view.vue";
+import KitchensView from "./kitchens-view.vue";
+import OrdersView from "./orders-view.vue";
+import ProductsView from "./products-view.vue";
+import RecipesView from "./recipes-view.vue";
+import ReservationsView from "./reservations-view.vue";
+import TablesView from "./tables-view.vue";
+import TransactionsView from "./transactions-view.vue";
+import UserView from "./user-view.vue";
+
+export {
+  HomeView,
+  ExpensesView,
+  FinancesView,
+  InventoryView,
+  ItemsView,
+  KitchensView,
+  OrdersView,
+  ProductsView,
+  RecipesView,
+  ReservationsView,
+  TablesView,
+  TransactionsView,
+  UserView,
+}
