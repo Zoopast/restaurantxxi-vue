44e1ef37ebad900da213f84e753bf5b213591033
Gabriel San Martin <gsandaur@hotmail.com>
2022-11-03 19:46:00 -0300

    feat(recipes): add recipes routes for show and update

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,12 +5,32 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def show
+    @ingredients = @recipe.ingredients
+    recipe = {
+      recipe: @recipe,
+      ingredients: @ingredients
+    }
+    render json: recipe
+  end
+
+  def update_recipe
+    @recipe = current_recipe
+    if @recipe.update(recipe_params)
+      edit_ingredients
+      redirect_to recipes_path
+    else
+      render json: @recipe.errors, status: :unprocessable_entity
+    end
+  end
+
   def new
     @recipe = Recipe.new
   end
 
   def create
     @recipe = Recipe.new(recipe_params)
+
     if @recipe.save
       create_ingredients
       redirect_to recipes_path
@@ -26,9 +46,25 @@ class RecipesController < ApplicationController
 
   private
 
+  def edit_ingredients
+    ingredients = params[:ingredients].values
+    current_ingredients = current_recipe.ingredients.collect {  |i| i.id }
+
+    ingredients.each do |ingredient|
+      if ingredient[:id].present? && current_ingredients.include?(ingredient[:id])
+        @ingredient = Ingredient.find(ingredient[:id])
+        @ingredient.update!({ name: ingredient[:name], qty: ingredient[:quantity], unit: ingredient[:unit] })
+      else
+
+        binding.pry
+
+        Ingredient.find(ingredient[:id]).destroy!
+      end
+    end
+  end
+
   def create_ingredients
     ingredients = params[:ingredients].values
-
     ingredients.each do |ingredient|
       @recipe.ingredients.create!(name: ingredient[:name],
                                   qty: ingredient[:quantity],
@@ -39,6 +75,10 @@ class RecipesController < ApplicationController
 
   def set_recipe
     @recipe = Recipe.find(params[:id])
+  end
+
+  def current_recipe
+    @recipe ||= Recipe.find(params[:recipe][:id])
   end
 
   def recipe_params
