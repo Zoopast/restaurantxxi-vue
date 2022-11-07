607db5a1ced2552c9d764d27c4ff400bd8e05281
Gabriel San Martin <gsandaur@hotmail.com>
2022-11-07 18:26:07 -0300

    feat(devise): add custom devise controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :recipes
   post 'recipes/update_recipe', to: 'recipes#update_recipe'
-    #controllers: {
-    #  registrations: 'users/registrations',
-    #  sessions: 'users/sessions'
-    # }
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
 end
