7a1d5404fbc84f5bad722636131d4de73457e61f
Gabriel San Martin <gsanmartinandaur@gmail.com>
2022-11-12 20:07:59 -0300

    chore(api): delete models controllers for api

--- app/controllers/api/v1/base_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::BaseController < Api::BaseController
-  skip_before_action :verify_authenticity_token
-  before_action :authenticate_user!
-
-  before_action do
-    self.namespace_for_serializer = ::Api::V1
-  end
-end
