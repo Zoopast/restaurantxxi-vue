ae1e6364438db366ad2bf5214992d3f5d9e8433a
Gabriel San Martin <gsanmartinandaur@gmail.com>
2022-11-19 18:52:33 -0300

    chore(clients): overrides devise sign up methods for sign in and routing

--- app/controllers/clients/registrations_controller.rb
+++ app/controllers/clients/registrations_controller.rb
@@ -1,2 +1,18 @@
 class Clients::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+  end
+
+  def sign_up(resource_name, resource)
+    #sign_in(resource_name, resource)
+  end
+
+  def after_sign_up_path_for(resource)
+    '/customers'
+  end
 end
