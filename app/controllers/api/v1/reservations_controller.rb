50b52ddfa5e2939e2e7cbae1336f805edd4ecdf1
Gabriel San Martin <gsanmartinandaur@gmail.com>
2022-11-26 14:37:51 -0300

    refactor(api/reservations): change reservation api creation"

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -21,8 +21,6 @@ class Api::V1::ReservationsController < ApplicationController
     @reservation = Reservation.new(reservation_params)
 
     if @reservation.save
-
-      binding.pry
       render json: @reservation, status: :created, location: api_v1_reservations_url(@reservation)
     else
       render json: @reservation.errors, status: :unprocessable_entity
@@ -39,7 +37,13 @@ class Api::V1::ReservationsController < ApplicationController
   private
 
   def reservation_params
-    params.require(:reservation).permit(:reservation_datetime, :status, :client_id, :table_id)
+    params.require(:reservation)
+          .permit(:reservation_datetime, :status, :client_id, :number_of_people)
+          .merge(table_id: find_table(params[:reservation][:number_of_people]))
+  end
+
+  def find_table(number_of_people)
+    Table.where("seats >= ?", number_of_people).first.id
   end
 
   def set_reservation
