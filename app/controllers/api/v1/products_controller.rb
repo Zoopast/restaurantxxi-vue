8883bd6c60c991350b02844f0ec41478ade367b0
Gabriel San Martin <gsanmartinandaur@gmail.com>
2022-11-26 21:30:07 -0300

    feat(api/products): discount qty from stock when product is created

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -5,6 +5,7 @@ class Api::V1::ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
+      reduce_inventory
       render json: @product, status: :created, location: api_v1_product_url(@product)
     else
       render json: @product.errors, status: :unprocessable_entity
@@ -15,5 +16,13 @@ class Api::V1::ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:recipe_id, :order_id)
+  end
+
+  def reduce_inventory
+    @product.recipe.ingredients.each do |ingredient|
+      item = Item.find_by(name: ingredient.name)
+      item.stock -= ingredient.qty
+      item.save
+    end
   end
 end
